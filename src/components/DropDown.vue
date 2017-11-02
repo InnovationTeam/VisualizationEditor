971163803eacd8b459aad36578107227847a37d0
AsanCai <969850420@qq.com>
2017-11-02 20:45:33 +0800

    创建左侧菜单栏

---
+++ src/components/DropDown.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="header_menu">
+        <div @click="show">
+            <i :class="[active?'el-icon-arrow-down':'el-icon-arrow-right']"></i>{{message}}
+        </div>
+
+        <div v-show="active" :class="container">
+
+        </div>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: 'dropDown',
+        props: ['message', 'container'],
+        data () {
+            return {
+                active: false
+            }
+        },
+        methods: {
+            show: function(){
+                this.active = !this.active;
+            }
+        }
+    }
+</script>
+
+<style>
+    .header_menu{
+		text-align: left;
+		background-color: #555555;
+		width: 100%;
+		height: 20px;
+		font-size: 12px;
+	}
+</style>
---
+++ src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+// 引入element-ui
+import ElementUI from 'element-ui'
+// css样式需要单独引入
+import 'element-ui/lib/theme-chalk/index.css'
+// 加载并渲染App
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+// 在Vue中使用elemrnt-ui
+Vue.use(ElementUI)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App }
+})
