a46f2c08365304e9623ea6c9b6f03d77a3794294
AsanCai <969850420@qq.com>
2017-11-26 19:32:36 +0800

    11月26日更新
    一堆bug

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
     <router-view></router-view>
+    <editor></editor>
   </div>
+
+  
 </template>
 
 <script>
+  import editor_js from "./components/LandingPage"
+
   export default {
-    name: 'my-project'
+    name: 'my-project',
+    components: {
+      'editor': editor_js
+    }
   }
 </script>
 
