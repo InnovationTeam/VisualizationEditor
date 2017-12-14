acb1e9954a9524ae6beac7e04560575c36c135b4
jackie-bryant <a871645081@qq.com>
2017-12-14 17:19:40 +0800

    无关的改动

--- src/api/File/saveFile.js
+++ src/api/File/saveFile.js
@@ -1,18 +1,13 @@
 
-function saveFile(self)
+
+export function saveFile(self)
 {
-    var vm=self.$children[0];//获取当前编辑器指针
-    var editor = vm.editor;
-
-    var content=editor.getValue();//获取当前编辑器内容
-
-    var filepath=self.filePath;//获取当前编辑器所打开的文件的路径
+    var content=self.$refs.editor1.get();
+    var filepath=self.filePath;
     var fs = require('fs');
     console.log(content); 
     fs.writeFile(filepath,content,function(err){
         if(err) throw err;
     });
-}
-module.exports = {
-    saveFile: saveFile
+
 }
